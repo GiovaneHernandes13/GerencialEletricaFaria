2311bcb0e7edddab9e371ae9ab6d2cf17fa801ff
GiovaneHenrnades <giovan3e@gmail.com>
2024-09-02 20:32:14 -0300

    ModelProduct

---
+++ GerencialEletricaFaria/app/Models/Produto.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id_produto',
+        'nome_produto',
+        'descricao',
+        'preco',
+        'estoque',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'preco' => 'decimal:2',
+        ];
+    }
+}
---
+++ GerencialEletricaFaria/database/migrations/2024_09_01_173256_create_produtos2_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id_produto'); // Define a coluna `id_produto` como chave primária e auto-incremento
+            $table->string('nome_produto', 100)->nullable(); // Coluna `nome_produto` do tipo varchar(100)
+            $table->text('descricao')->nullable(); // Coluna `descricao` do tipo text
+            $table->decimal('preco', 10, 2)->nullable(); // Coluna `preco` do tipo decimal(10,2)
+            $table->integer('estoque')->nullable(); // Coluna `estoque` do tipo int(11)
+            $table->string('nova_coluna', 255)->nullable(); // Coluna `nova_coluna` do tipo varchar(255)
+            $table->timestamps(); // Adiciona as colunas `created_at` e `updated_at` automaticamente
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
