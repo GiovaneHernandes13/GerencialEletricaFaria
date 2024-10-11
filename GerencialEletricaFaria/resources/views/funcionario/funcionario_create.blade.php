6fa4bddf8749105e0eddc30a8f890bfab752c2df
GiovaneHenrnades <giovan3e@gmail.com>
2024-10-11 19:10:36 -0300

    ViewsFuncionarios

--- GerencialEletricaFaria/resources/views/funcionario/funcionario_create.blade.php
+++ GerencialEletricaFaria/resources/views/funcionario/funcionario_create.blade.php
@@ -3,14 +3,39 @@
 @section('conteudo')
     <form action="{{ route('funcionarios.store') }}" method="POST">
         @csrf
-        <input type="text" name="nome" placeholder="Nome" required>
-        <input type="text" name="cpf" placeholder="CPF" required>
-        <input type="text" name="rg" placeholder="RG" required>
-        <input type="text" name="telefone" placeholder="Telefone" required>
-        <input type="email" name="email" placeholder="E-mail" required>
-        <input type="date" name="data_contratacao" placeholder="Data de Contratação" required>
-        <input type="text" name="cargo" placeholder="Cargo" required>
-        <input type="number" step="0.01" name="salario" placeholder="Salário" required>
+        
+        <div>
+            <label for="">Nome</label>
+            <input type="text" name="nome" placeholder="Nome" required>
+        </div>
+        <div>
+            <label for="">CPF</label>
+            <input type="text" name="cpf" placeholder="000.000.000-00" required>
+        </div>
+        <div>
+            <label for="">RG</label>
+            <input type="text" name="rg" placeholder="RG" required>
+        </div>
+        <div>
+            <label for="">Telefone</label>
+            <input type="text" name="telefone" placeholder="Telefone" required>
+        </div>
+        <div>
+            <label for="">E-mail</label>
+            <input type="email" name="email" placeholder="E-mail" required>
+        </div>
+        <div>
+            <label for="">Data de Contratação</label>
+            <input type="date" name="data_contratacao" placeholder="Data de Contratação" required>
+        </div>
+        <div>
+            <label for="">Cargo</label>
+            <input type="text" name="cargo" placeholder="Cargo" required>
+        </div>
+        <div>
+            <label for="">salario</label>
+            <input type="number" step="0.01" name="salario" placeholder="Salário" required>
+        </div>
         <button type="submit">Salvar</button>
     </form>
 @endsection
